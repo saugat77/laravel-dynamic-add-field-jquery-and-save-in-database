e07cb48527d7635a1524deb818e0c47327511228
= <saugatpandey4@gmail.com>
2022-08-26 14:45:55 +0545

    first commit

---
+++ app/Http/Controllers/DynamicFieldController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\DynamicField;
+use Validator;
+
+class DynamicFieldController extends Controller
+{
+    function index()
+    {
+     return view('dynamic_field');
+    }
+
+    function insert(Request $request)
+    {
+     if($request->ajax())
+     {
+      $rules = array(
+       'first_name.*'  => 'required',
+       'last_name.*'  => 'required'
+      );
+      $error = Validator::make($request->all(), $rules);
+      if($error->fails())
+      {
+       return response()->json([
+        'error'  => $error->errors()->all()
+       ]);
+      }
+
+      $first_name = $request->first_name;
+      $last_name = $request->last_name;
+      for($count = 0; $count < count($first_name); $count++)
+      {
+       $data = array(
+        'first_name' => $first_name[$count],
+        'last_name'  => $last_name[$count]
+       );
+       $insert_data[] = $data;
+      }
+
+      DynamicField::insert($insert_data);
+      return response()->json([
+       'success'  => 'Data Added successfully.'
+      ]);
+     }
+    }
+}
---
+++ resources/views/dynamic_field.blade.php
@@ -0,0 +1,110 @@
+<html>
+ <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Dynamic field</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+  <div class="container">
+     <br />
+     <h3 align="center">Dynamically Add / Remove input fields </h3>
+     <br />
+   <div class="table-responsive">
+                <form method="post" id="dynamic_form">
+                 <span id="result"></span>
+                 <table class="table table-bordered table-striped" id="user_table">
+               <thead>
+                <tr>
+                    <th width="35%">First Name</th>
+                    <th width="35%">Last Name</th>
+                    <th width="30%">Action</th>
+                </tr>
+               </thead>
+               <tbody>
+
+               </tbody>
+               <tfoot>
+                <tr>
+                                <td colspan="2" align="right">&nbsp;</td>
+                                <td>
+                  @csrf
+                  <input type="submit" name="save" id="save" class="btn btn-primary" value="Save" />
+                 </td>
+                </tr>
+               </tfoot>
+           </table>
+                </form>
+   </div>
+  </div>
+ </body>
+</html>
+
+<script>
+$(document).ready(function(){
+
+ var count = 1;
+
+ dynamic_field(count);
+
+ function dynamic_field(number)
+ {
+  html = '<tr>';
+        html += '<td><input type="text" name="first_name[]" class="form-control" /></td>';
+        html += '<td><input type="text" name="last_name[]" class="form-control" /></td>';
+        if(number > 1)
+        {
+            html += '<td><button type="button" name="remove" id="" class="btn btn-danger remove">Remove</button></td></tr>';
+            $('tbody').append(html);
+        }
+        else
+        {
+            html += '<td><button type="button" name="add" id="add" class="btn btn-success">Add</button></td></tr>';
+            $('tbody').html(html);
+        }
+ }
+
+ $(document).on('click', '#add', function(){
+  count++;
+  dynamic_field(count);
+ });
+
+ $(document).on('click', '.remove', function(){
+  count--;
+  $(this).closest("tr").remove();
+ });
+
+ $('#dynamic_form').on('submit', function(event){
+        event.preventDefault();
+        $.ajax({
+            url:'{{ route("dynamic-field.insert") }}',
+            method:'post',
+            data:$(this).serialize(),
+            dataType:'json',
+            beforeSend:function(){
+                $('#save').attr('disabled','disabled');
+            },
+            success:function(data)
+            {
+                if(data.error)
+                {
+                    var error_html = '';
+                    for(var count = 0; count < data.error.length; count++)
+                    {
+                        error_html += '<p>'+data.error[count]+'</p>';
+                    }
+                    $('#result').html('<div class="alert alert-danger">'+error_html+'</div>');
+                }
+                else
+                {
+                    dynamic_field(1);
+                    $('#result').html('<div class="alert alert-success">'+data.success+'</div>');
+                }
+                $('#save').attr('disabled', false);
+            }
+        })
+ });
+
+});
+</script>
